c73e207d866190c64e1ed0104f8ca34932ce07dc
Calum Murray <cm2103@hw.ac.uk>
2024-03-07 01:33:47 +0000

    CW1 PreCommit

--- Semester2/Hardware-Software Interface/CW1/cwk1-c/process.c
+++ Semester2/Hardware-Software Interface/CW1/cwk1-c/process.c
@@ -26,18 +26,24 @@ struct Image {
     struct Pixel *pixelArray;
 };
 
+// struct ImageList {
+//     char input[];
+//     char output[];
+// };
+
 /* Free a struct Image */
 void free_image(struct Image *img)
 {
     /* TODO: Question 2a */
     free(img);
+    free(img->pixelArray);
 }
 
 /* Opens and reads an image file, returning a pointer to a new struct Image.
  * On error, prints an error message and returns NULL. */
 struct Image *load_image(const char *filename)
 {
-    fprintf(stdout, "->load_image initiated \n");
+    fprintf(stdout, "LOAD INITIATED\n");
     /* Open the file for reading */
     FILE *f = fopen(filename, "rb");
     if (f == NULL) {
@@ -56,26 +62,26 @@ struct Image *load_image(const char *filename)
 
 
     //scans header
-    if (fscanf(f, "%3s", &Image->file_type) != 1) {
+    if (fscanf(f, "%3s", Image->file_type) != 1) {
         fprintf(stderr, "File Header couldn't be read %s\n", filename);
         fclose(f);
         return NULL;
     }
-    fprintf(stdout, "Image Type %s\n", Image->file_type);
+    //fprintf(stdout, "Image Type %s\n", Image->file_type);
 
     if (fscanf(f, "%d", &Image->width) != 1) {
         fprintf(stderr, "File Width couldn't be read %s\n", filename);
         fclose(f);
         return NULL;
     }
-    fprintf(stdout, "Image Width %d\n", Image->width);
+    //fprintf(stdout, "Image Width %d\n", Image->width);
 
     if (fscanf(f, "%d", &Image->height) != 1) {
         fprintf(stderr, "File Height couldn't be read %s\n", filename);
         fclose(f);
         return NULL;
     }
-    fprintf(stdout, "Image Height %d\n", Image->height);
+    //fprintf(stdout, "Image Height %d\n", Image->height);
 
     //kills \n's
     fgetc(f);
@@ -103,36 +109,6 @@ struct Image *load_image(const char *filename)
         }
     }
 
-
-/*
-    char data[9];
-    size_t bytes_read;
-    for (int i = 0; i <3; i++) {
-        bytes_read = fread(data, 1, 8, f);
-        if (bytes_read != 8) {
-            fprintf(stdout, "error reading line %d \n", i + 1);
-            break;
-    }
-    data[8] = '\0';
-    fprintf(stdout, "Line %d: %s\n", i+1, data);    
-}
-*/
-/*
-    if (fread(&data, sizeof(unsigned char), 1, f) != 8) {
-        fprintf(stderr, "File data couldn't be read %d\n", f);
-        fclose(f);
-        return(NULL);
-    }
-    for (int i = 7; i >= 0; i--) {
-        printf("%d \n", (data >> i) & 1);
-    }
-*/
-
-
-    //allocates space based on read header
-    //Image.pixelArray = (struct Pixel)malloc();
-
-
     /* Close the file */
     fclose(f);
 
@@ -140,7 +116,7 @@ struct Image *load_image(const char *filename)
         fprintf(stderr, "File %s could not be read.\n", filename);
         return NULL;
     }
-    fprintf(stdout, "->load_image complete \n");
+    fprintf(stdout, "LOAD COMPLETE\n");
     return Image;
 }
 
@@ -158,20 +134,20 @@ void printPixelValues(struct Image *image) {
 /* Write img to file filename. Return true on success, false on error. */
 bool save_image(const struct Image *img, const char *filename)
 {
-    fprintf(stdout, "->save_image initiated \n");
+    fprintf(stdout, "SAVE INITIATED\n");
     /* TODO: Question 2c */
     //takes image structure and parses through as it writes data to a file using 
     FILE *f = fopen(filename, "wb");
-    fprintf(stdout, "file opened successfully \n");
+    //fprintf(stdout, "file opened successfully \n");
     
     fprintf(f, "%s\n", img->file_type);
-    fprintf(stdout, "file_type written: %s \n", img->file_type);
+    //fprintf(stdout, "file_type written: %s \n", img->file_type);
     
     fprintf(f, "%d ", img->width);
-    fprintf(stdout, "width written: %d \n", img->width);
+    //fprintf(stdout, "width written: %d \n", img->width);
     
     fprintf(f, "%d\n", img->height);
-    fprintf(stdout, "height written: %d \n", img->height);
+    //fprintf(stdout, "height written: %d \n", img->height);
     //this is shoddily put together, must be made robust ^^^^
 
 //  fprintf(NameOfFilePointer, "%d", myIntegerVariableName);
@@ -181,34 +157,32 @@ bool save_image(const struct Image *img, const char *filename)
 //  similar to fscan/fread paradigm
 
     //now the grunt work: writing binary data from IMAGE struct with fwrite
-    /* ATTEMPT1, with fwrite
+    
     for (int i = 0; i < (img->width * img->height); i++) {
-        fprintf(stdout, "pixel:%d \n", i);
-        fwrite(img->pixelArray[i].red, sizeof(unsigned char), 1, f);
-        fprintf(stdout, "pixel:%d \n", i);
-        fwrite(img->pixelArray[i].blue, sizeof(unsigned char), 1, f);
-        fwrite(img->pixelArray[i].green, sizeof(unsigned char), 1, f);
+        //fprintf(stdout, "pixel:%d - r%c g%c b%c \n", i, &img->pixelArray[i].red, &img->pixelArray[i].green, &img->pixelArray[i].blue);
+        fwrite(&img->pixelArray[i].red, sizeof(unsigned char), 1, f);
+        fwrite(&img->pixelArray[i].blue, sizeof(unsigned char), 1, f);
+        fwrite(&img->pixelArray[i].green, sizeof(unsigned char), 1, f);
     }
-    */
+    
    
-   // ATTEMPT2, uses fprintf to print decimals
-    for (int i = 0; i < (img->width * img->height); i++) {
-        if (fprintf(f, "%c", img->pixelArray[i].red) != 1 ||
-        fprintf(f, "%c", img->pixelArray[i].green) != 1 ||
-        fprintf(f, "%c", img->pixelArray[i].blue) != 1 ) {
+//    // ATTEMPT2, uses fprintf to print decimals
+//     for (int i = 0; i < (img->width * img->height); i++) {
+//         if (fprintf(f, "%c", img->pixelArray[i].red) != 1 ||
+//         fprintf(f, "%c", img->pixelArray[i].green) != 1 ||
+//         fprintf(f, "%c", img->pixelArray[i].blue) != 1 ) {
 
-        fprintf(stderr, "failed to save pixel data on pixel:%d \n", i);
-        return false;
-        }
-    }
+//         fprintf(stderr, "failed to save pixel data on pixel:%d \n", i);
+//         return false;
+//         }
+//     }
 
     // PIXEL DATA IS CONVERTED TO CHARACTERS OF DECIMAL INTS, THUS I NEED TO EITHER CAST THEM HERE  OR  
-
 
     fclose(f);
 
     //implement error returns on wites and saves
-    fprintf(stdout, "->save_image complete \n");
+    fprintf(stdout, "SAVE COMPLETE\n");
     return true;
 }
 
@@ -219,20 +193,27 @@ struct Image *copy_image(const struct Image *source)
     /* TODO: Question 2d */
     struct Image *img_copy = malloc(sizeof(*source));
     
-    img_copy = source;
+    strcpy(img_copy->file_type, source->file_type);
+    //fprintf(stdout, "%s \n", img_copy->file_type);
+    img_copy->width = source->width;
+    img_copy->height = source->height;
+    //fprintf(stdout, "%d %d \n", img_copy->width, img_copy->height);
 
-    img_copy->pixelArray = (struct Pixel *)malloc(sizeof(source->pixelArray));
-
-    for (int i = 0; i < (img_copy->width * img_copy->height); i++){
-        img_copy->pixelArray[i].red = source->pixelArray[i].red;
-        //fprintf(stdout, "red%d: %c, %c \n", i, img_copy->pixelArray[i].red, source->pixelArray[i].red);
-
-    }
+    
+    //Image->pixelArray
+    img_copy->pixelArray = (struct Pixel *)malloc(img_copy->width * img_copy->height * sizeof(struct Pixel));
 
 
-    free(img_copy);
-    free(img_copy->pixelArray);
-    return NULL;
+    memcpy(img_copy->pixelArray, source->pixelArray, img_copy->width * img_copy->height * sizeof(struct Pixel));
+    // for (int i = 0; i < (img_copy->width * img_copy->height); i++) {
+    //     img_copy->pixelArray[i].red = source->pixelArray[i].red;
+    //     //fprintf(stdout, "%c\n", img_copy->pixelArray[i].red);
+    //     //fprintf(stdout, "%d\n", i);
+    // }
+
+    //fprintf(stdout, "%s\n", img_copy->file_type);
+
+    return img_copy;
 }
 
 /* Perform your first task.
@@ -242,12 +223,113 @@ struct Image *copy_image(const struct Image *source)
 struct Image *apply_MEDIAN(const struct Image *source)
 {
     /* TODO: Question 3 */
-    return NULL;
-
+    //return NULL;
+    fprintf(stdout, "MEDIAN INITIATED\n");
     //pointer at a pixel changes colour value, to median of the 4 adjacent pixels, above/below/right/left
     //excluding nonexistent pixels, when at edges
+    struct Image *img_median = copy_image(source);
 
 
+
+    int pixel = 0;
+
+        for (int h = 0; h < source->height; h++) {
+            bool hs, he = false;
+            if (h == 0) {
+                hs = true;
+            } else if (h == (source->height - 1)) {
+                he = true;
+            }
+
+            for (int w = 0; w < source->width; w++) {
+                bool ws, we = false;
+                if (w == 0)  {
+                    ws = true;
+                } else if (w == (source->width - 1)) {
+                    we = true;
+                }
+
+                pixel = (w) + (h * source->width);
+
+
+                // fprintf(stdout, "pixel:%d, w/h:%d/%d, %u %u %u \n", pixel, w, h, p.r, p.g, p.b);
+                int count = 4;
+                if (hs) count--;
+                if (he) count--;
+                if (ws) count--;
+                if (we) count--;
+
+
+
+                        int compare(const void *a, const void *b) {
+                            return (*(int *)a - *(int *)b);
+                        }   
+
+
+                //////////////////////////////////////////////////////////////////////////////
+                int red[count];
+                int green[count];
+                int blue[count];
+        
+
+                int c = 0;
+                red[0] = source->pixelArray[pixel].red;
+                green[0] = source->pixelArray[pixel].green;
+                blue[0] = source->pixelArray[pixel].blue;
+                c++;
+
+                if (!hs) {
+                    red[c] = source->pixelArray[pixel - source->width].red;
+                    green[c] = source->pixelArray[pixel - source->width].green;
+                    blue[c] = source->pixelArray[pixel - source->width].blue;
+                    c++;
+                }
+                if (!he) {
+                    red[c] = source->pixelArray[pixel + source->width].red;
+                    green[c] = source->pixelArray[pixel + source->width].green;
+                    blue[c] = source->pixelArray[pixel + source->width].blue;
+                    c++;
+                }
+                if (!ws) {
+                    red[c] = source->pixelArray[pixel - 1].red;
+                    green[c] = source->pixelArray[pixel - 1].green;
+                    blue[c] = source->pixelArray[pixel - 1].blue;
+                    c++;
+                }
+                if (!we) {
+                    red[c] = source->pixelArray[pixel + 1].red;
+                    green[c] = source->pixelArray[pixel + 1].green;
+                    blue[c] = source->pixelArray[pixel + 1].blue;
+                    c++;
+                }
+
+                qsort(red, c, sizeof(int), compare);
+                qsort(green, c, sizeof(int), compare);
+                qsort(blue, c, sizeof(int), compare);
+
+                int redMedian, blueMedian, greenMedian = 0;
+                if (c % 2 != 0) {
+                    redMedian = red[c / 2];
+                    greenMedian = green[c / 2];
+                    blueMedian = blue[c / 2];
+                } else {
+                    redMedian = (red[(c - 1) / 2] + red[c / 2]) / 2.0;
+                    greenMedian = (green[(c - 1) / 2] + green[c / 2]) / 2.0;
+                    blueMedian = (blue[(c - 1) / 2] + blue[c / 2]) / 2.0;
+                }
+
+                img_median->pixelArray[pixel].red = redMedian;
+                img_median->pixelArray[pixel].green = greenMedian;
+                img_median->pixelArray[pixel].blue = blueMedian;
+
+                
+
+            }
+        }
+
+
+    fprintf(stdout, "MEDIAN COMPLETE\n");
+    return img_median;
 
 }
 
@@ -255,23 +337,75 @@ struct Image *apply_MEDIAN(const struct Image *source)
  * (TODO: Write a better comment here, and rename the function.
  * You may need to add or change arguments depending on the task.)
  * Returns true on success, or false on error. */
-bool apply_NORM(const struct Image *source)
+bool apply_NORM(struct Image *source)
 {
     /* TODO: Question 4 */
-    return false;
-
+    fprintf(stdout, "NORM INITIATED\n");
     //scan for min and max for each colour values in the image
-    //subtract the offset minimums, to 0
+    //subtract the offset minimums, to 0   // to all
     //then multiply all values by a scaling factor so the max values are scaled to the max possible values
+    int min = 255;
+    int max = 0;
+
+    for (int i = 0; i < source->width*source->height; i++) {
+        if (source->pixelArray[i].red < min){
+            min = source->pixelArray[i].red;
+        }
+        if (source->pixelArray[i].red > max){
+            max = source->pixelArray[i].red;
+        }
+
+        if (source->pixelArray[i].green < min){
+            min = source->pixelArray[i].green;
+        }
+        if (source->pixelArray[i].green > max){
+            max = source->pixelArray[i].green;
+        }
+
+        if (source->pixelArray[i].blue < min){
+            min = source->pixelArray[i].blue;
+        }
+        if (source->pixelArray[i].blue > max){
+            max = source->pixelArray[i].blue;
+        }
+    }    
+
+    int offset = min;
+    float scaling_factor = (float)255 / (max-min);
+
+    for (int i = 0; i < (source->width*source->height); i++) {
+        int red = (int)((source->pixelArray[i].red - offset) * scaling_factor);
+        int green = (int)((source->pixelArray[i].green - offset) * scaling_factor);
+        int blue = (int)((source->pixelArray[i].blue - offset) * scaling_factor);
+
+        // Check if the values are out of bounds
+        red = red < 0 ? 0 : (red > 255 ? 255 : red);
+        green = green < 0 ? 0 : (green > 255 ? 255 : green);
+        blue = blue < 0 ? 0 : (blue > 255 ? 255 : blue);
+
+        source->pixelArray[i].red = (unsigned char)red;
+        source->pixelArray[i].green = (unsigned char)green;
+        source->pixelArray[i].blue = (unsigned char)blue;
+    }    
+
+
+    for (int i = 0; i < (source->width*source->height); i++) {
+        if (source->pixelArray[i].red == 0 || source->pixelArray[i].green == 0 || source->pixelArray[i].blue == 0){
+            fprintf(stdout,"0 found\n");
+        }
+        if (source->pixelArray[i].red == 255 || source->pixelArray[i].green == 255 || source->pixelArray[i].blue == 255){
+            fprintf(stdout,"255 found\n");
+        }
+    }
+    
+    fprintf(stdout, "NORM COMPLETE\n");
+    return true;
 
 
 }
 
 int main(int argc, char *argv[])
 {
-    /* Initialise the random number generator, using the time as the seed */
-    /* TODO: Remove this if you don't need to use rand() */
-    srand(time(NULL));
 
     /* Check command-line arguments */
     if (argc != 3) {
@@ -279,23 +413,21 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    //int images = (argc-1)/2;
+
+
+    
+    
+
+
+
     /* Load the input image */
     struct Image *in_img = load_image(argv[1]);
     if (in_img == NULL) {
         return 1;
     }
-
-    if (!save_image(in_img, argv[2])) {
-        fprintf(stderr, "saving image failed \n");
-    }
-    
+    save_image(in_img, "1stsave.hs8");
     // printPixelValues(in_img);
-
-    copy_image(in_img);
-
-
-
-
 
     /* Apply the first process */
     struct Image *out_img = apply_MEDIAN(in_img);
@@ -304,6 +436,7 @@ int main(int argc, char *argv[])
         free_image(in_img);
         return 1;
     }
+    save_image(in_img, "2ndsave.hs8");
 
     /* Apply the second process */
     if (!apply_NORM(out_img)) {
